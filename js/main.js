bd5c26291910a362bee7e6972fec7b882bbe55fd
Galina Shvets <galina@gmail.com>
2019-08-01 19:46:45 +0300

    - add style header and banner

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+jQuery(function ($) {
+    $('.sl').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // speed: 1000,
+        slidesToShow: 1,
+        infinite: true,
+        arrows: true, /*стрілки керування*/
+    });
+
+});
